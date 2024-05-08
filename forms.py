0e01f1dfc20d1978c95b12df052fadd54be637d9
Zach Bunting <zvrbunting@gmail.com>
2024-05-08 13:14:02 -0700

    Added CSRF and EditProfile Forms

--- forms.py
+++ forms.py
@@ -45,3 +45,38 @@ class LoginForm(FlaskForm):
         'Password',
         validators=[InputRequired(), Length(min=6, max=50)],
     )
+
+
+class CSRFForm(FlaskForm):
+    """CSRF form used to log out"""
+
+
+class EditProfile(FlaskForm):
+    """Form for editing a profile"""
+
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    email = StringField(
+        'E-mail',
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+
+    image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    header_image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    bio = TextAreaField('text', validators=[Optional()])
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
+    )
